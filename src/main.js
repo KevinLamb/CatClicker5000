907e39aab316bf333a0a7eab6e4165b4ed1cad70
Eros <eros.dipede@cloud.statcan.ca>
2018-10-24 16:26:35 +0100

    Enhance game loop, score added, game speeds up on clicks

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@
 var width = window.innerWidth;
 var height = window.innerHeight;
 var interval = 7500;
+var minInterval = 500;
+var intervalScalar = 5;
 var catCount = 0;
+var currFrame = 0;
 
 //creates the stage
 var stage = new Konva.Stage({
@@ -11,26 +14,15 @@ var stage = new Konva.Stage({
   height: height
 });
 
-//creates layer
-var layer = new Konva.Layer();
-// add the layer to the stage
-stage.add(layer);
-
-//loads cat image
-var catImage = new Image();
-catImage.src = './assets/cat1.png';
-//initializing function
-catImage.onload = function() {
-  //begins game
-  window.setInterval(function() {update(); stage.add(layer)}, interval);
-  // add the layer to the stage
-  stage.add(layer);
-};
-
-//update function, updates game entities
-var update = () =>{
-  createCat();
-}
+//creates score text
+var score = new Konva.Text({
+  x: stage.getWidth() / 2,
+  y: 20,
+  text: '0',
+  fontSize: 40,
+  fontFamily: 'Calibri',
+  fill: 'black'
+});
 
 //creates a cat object
 function createCat() {
@@ -48,11 +40,45 @@ function createCat() {
   catObject.on('click', function(evt) {
     this.destroy();
     catCount += 1;
-    layer.draw();
-
-    console.log(catCount);
   });
 
   //adds cat to layer
   layer.add(catObject);
+}
+
+//creates layer
+var layer = new Konva.Layer();
+//adds score to layer
+layer.add(score)
+// add the layer to the stage
+stage.add(layer);
+
+//loads cat image
+var catImage = new Image();
+catImage.src = './assets/cat1.png';
+//initializing function
+catImage.onload = function() {
+  //begins game
+  loop();
+};
+
+//update function, updates game entities
+var update = () => {
+  interval = (interval >= minInterval) ? interval - (catCount * intervalScalar) : minInterval;
+  if (currFrame >= interval){
+    createCat();
+    currFrame = 0;
+  }
+  console.log(currFrame++);
+  score.setAttr('text', catCount);
+}
+//draws entities
+var draw = () => {
+  layer.draw();
+}
+//game loop
+function loop() {
+  update()
+  draw()
+  setInterval(loop,60)
 }
